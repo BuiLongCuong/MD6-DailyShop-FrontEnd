62f7ae33172127cddf7fb27167512cdc6ca2b97f
unknown <phamvanhuan.bk@gmail.com>
2024-01-04 19:27:49 +0900

    HomeSimple lan 1

--- src/pages/HomeSimple.js
+++ src/pages/HomeSimple.js
@@ -1,7 +1,7 @@
-import {Header} from "../components/Header";
+import Header from "../components/Header";
 import {Navbar} from "../components/Navbar";
 import {Outlet} from "react-router-dom";
-import {Footer} from "../components/Footer";
+import Footer from "../components/Footer";
 
 export default function HomeSimple() {
     return (
