5354b2b9b9e1e50ef1705c162ba71247951b784b
unknown <phamvanhuan.bk@gmail.com>
2024-01-09 10:56:58 +0900

    HomeSimple -9/1/2024

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,29 @@
+import "./Navbar.css"
 import {Link} from "react-router-dom";
 
 export function Navbar() {
     return (
         <>
-            <h1>
-                {/*<Link to={"/dailyShop"}>Home</Link>*/}
-                <Link to={'/login'}>Đăng nhập</Link> |
-                <Link to={'/signIn'}>Kênh người dùng</Link>
-            </h1>
+            <div className="nav-bar">
+                <div className="nav-bar-left">
+                    <img src="https://tailieutinhoc.com/wp-content/uploads/2023/09/backdrop-tet-nguyen-dan-6.jpg"
+                         alt=""/>
+                </div>
+                <div className="nav-bar-center">
+                    <ul>
+                        <li><Link to={"#"}>TRANG CHỦ</Link></li>
+                        <li><Link to={"#"}>CỬA HÀNG</Link></li>
+                        <li><Link to={"#"}>SẢN PHÂM</Link></li>
+                        <li><Link to={"#"}>BÀI VIẾT</Link></li>
+                        <li><Link to={"#"}>LIÊN HỆ</Link></li>
+                    </ul>
+                </div>
+                <div className="nav-bar-right">
+                    <img src="https://tailieutinhoc.com/wp-content/uploads/2023/09/backdrop-tet-nguyen-dan-6.jpg"
+                         alt=""/>
+                </div>
+
+            </div>
         </>
     )
 }
--- src/pages/HomeSimple.js
+++ src/pages/HomeSimple.js
@@ -2,12 +2,14 @@ import Header from "../components/Header";
 import {Navbar} from "../components/Navbar";
 import {Outlet} from "react-router-dom";
 import Footer from "../components/Footer";
+import Body from "../components/Body";
 
 export default function HomeSimple() {
     return (
         <>
             <Header/>
             <Navbar/>
+            <Body/>
             <Outlet/>
             <Footer/>
         </>
